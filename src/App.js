c9b1b54f4bffce68b7930ddc15a0addd3f91f79f
Felix Khafizov N <asssama212@gmail.com>
2018-08-15 19:46:30 +0300

    Made logo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
-import Navigation from "./components/Logo/Logo";
+import Logo from "./components/Logo/Logo";
 
 class App extends Component {
     render() {
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <nav style={{ textAlign: "right" }}>
+        <div style={{ textAlign: "right" }}>
             <p className="f3 link dim black underline pa3 pointer">Sign out</p>
-        </nav>
+        </div>
     );
 };
 
