664d2e68cb9d304e0cd9a801e9e620e8bb01af30
Felix Khafizov N <asssama212@gmail.com>
2018-08-15 21:17:47 +0300

    Made rank component and added particles background

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        },
+        number: {
+            value: 71
+        }
+    }
+};
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Particles params={particlesOptions} className="particles" />
                 <Navigation />
                 <Logo />
+                <Rank />
                 <ImageLinkForm />
                 {/*
           
