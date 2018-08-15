07bec7a6eee30db86793eaa4e57bb7e70d868096
Felix Khafizov N <asssama212@gmail.com>
2018-08-15 18:55:58 +0300

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav style={{ textAlign: "right" }}>
+            <p className="f3 link dim black underline pa3 pointer">Sign out</p>
+        </nav>
+    );
+};
+
+export default Navigation;
